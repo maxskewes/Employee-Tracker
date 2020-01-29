8ff8570c06dcad324e98ff3ef417703f1dbb396e
Max <maxskewes@gmail.com>
2020-01-28 21:10:34 -0800

    functional

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const consoleTable = require("console.table")
+const figlet = require("figlet");
+const awesomeBanner = require("awesome-banner");
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
@@ -24,15 +26,13 @@ connection.connect(function(err) {
 function questions() {
     inquirer.prompt({
        type: "list",
-       name: "response",
+       name: "choices",
        message: "What would you like to do?",
        choices: ["View all departments", "View all employees", "View all roles", "Add a department", "Add an employee", "Add a role", "Update employee roles", "Exit/Quit"]
     })
-};
 
-questions()
-.then(function(response) {
-    switch (response.options) {
+.then(function(answer) {
+    switch (answer.choices) {
         case "View all departments":
             viewAllDepts();
             break;
@@ -57,13 +57,133 @@ questions()
             addRole();
             break;
 
-        case "Update Employee roles":
-            updateEmpRoles();
+        case "Update employee roles":
+            updateRoles();
+            console.log("Case firing");
             break;
 
-        case "Exit/Quit":
+        case "Exit":
             quit();
             break;
     
     }
 });
+}
+
+questions();
+
+function viewAllDepts() {
+    connection.query("SELECT * FROM DEPARTMENTS", function(err, res)  {
+    if (err) throw err;
+    // console.log(
+    //     awesomeBanner.blue(
+    //       figlet.textSync("Departments", { horizontalLayout: "default" })
+    //     )
+    //);
+        console.table(res);
+    });
+questions();
+};
+
+function viewAllEmps()  {
+    console.log("employee");
+    connection.query("SELECT * FROM EMPLOYEES", function(err, res)  {
+        if (err) throw err;
+        console.table(res);
+    });
+questions();
+};
+
+function viewAllRoles() {
+    console.log("roles");
+    connection.query("SELECT * FROM ROLES", function(err, res)  {
+        if (err) throw err;
+        console.table(res);
+    });
+questions();
+};
+
+//add a department
+function addDept()  {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "dept",
+        message: "Enter the name of the department you would like to add"
+      }
+    ])
+    .then(function(answer){
+        console.log(answer);
+        connection.query("INSERT INTO DEPARTMENTS SET ?", {NAME:answer.dept}, function(err, res){
+            console.log(res)
+        })
+    })
+    .then(questions);
+};
+
+//add an employee
+function addEmp()  {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "emp",
+        message: "Enter the name of the employee you would like to add"
+      }
+    ])
+    .then(function(answer){
+        console.log(answer);
+        connection.query("INSERT INTO EMPLOYEES SET ?", {FIRST_NAME:answer.emp}, function(err, res){
+            console.log(res)
+        })
+    })
+    .then(questions)
+};
+
+//add A role
+function addRole()  {
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "role",
+        message: "Enter the name of the role you would like to add"
+      }
+    ])
+    .then(function(answer){
+        console.log(answer);
+        connection.query("INSERT INTO ROLES SET ?", {TITLE:answer.role}, function(err, res){
+            console.log(res)
+        })
+    })
+    .then(questions)
+};
+
+function updateRoles()  {
+    console.log("updateRoles FIRED")
+    inquirer
+    .prompt([
+        {
+        type: "input",
+        name: "empID",
+        message: "Enter the ID of the employee whose role you would like to update"
+      },
+      {
+        type: "input",
+        name: "newRole",
+        message: "Enter the new role you would like the employee to take on"
+      }
+    ])
+    .then(function(answer)  {
+        console.log(answer);
+        connection.query("UPDATE EMPLOYEES SET ? WHERE ?", {ROLES_ID:answer.newRole, ID:answer.empID}, function(err, res){
+            console.log("anyword")
+        })
+    }).then(questions);
+    
+};
+
+function quit() {
+    console.log("<<<YOU HAVE EXITED>>>")
+};
